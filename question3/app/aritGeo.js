85f64b3eca1520e809f2f80c173609b4fbc68382
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-09 23:00:12 +0100

    aritGeo modified and all test passed

--- question3/app/aritGeo.js
+++ question3/app/aritGeo.js
@@ -4,27 +4,24 @@ module.exports = {
 			if (!(numList instanceof Array)){
 				return "Input must be an array";
 			}
+			else if (numList.length == 0) {
+				return 0;
+			}
 
 			else{
 				for(var counter = 0;counter< numList.length-1; counter++){
-					if (numList.length > 0){
-						if ((numList[counter+1]-numList[counter]) === (numList[numList.length-1]-numList[numList.length-2])){
-							return "Arithmetic";
-						}
-						else if ((numList[counter+1]/numList[counter]) === (numList[counter+2]/numList[counter+1])){
-							return "Geometric";
-						}
-						else {
-							return -1;
-						}	
+					if ((numList[counter+1]-numList[counter]) === (numList[numList.length-1]-numList[numList.length-2])){
+						return "Arithmetic";
 					}
-					
-					else if (numList.length == 0) {
-						return 0;
+					else if ((numList[counter+1]/numList[counter]) === (numList[counter+2]/numList[counter+1])){
+						return "Geometric";
 					}
-					
+					else {
+						return -1;
+					}	
 				}
 			}
-	}
 
+		}
+	
 };
